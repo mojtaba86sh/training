601663feb8f5158b43812ef4b71298ea1ea7469a
mojtaba86sh <pcm46home@gmail.com>
2024-11-29 21:23:27 +0400

    Add files via upload

---
+++ magaleh.php
@@ -0,0 +1,9 @@
+<?php
+include("header.html")
+?>
+<p id="td">آموزش های بروز</p>
+<div class="srb3"><a class="a" href="magalehAMD.html">آموزش آپدیت کارت گرافیک AMD</a></div>
+<div class="srb3"><a class="a" href="magalehNVIDIA.html">آموزش آپدیت کارت گرافیک NVIDIA</a></div>
+<?php
+include("footer.html")
+?>
---
+++ news.php
@@ -0,0 +1,10 @@
+<?php
+include("header.html")
+?>
+<p id="td">اخبار امروز</p>
+<div class="srb3"><a class="a" href="newsi9.html">جدید ترین پردازنده Intel</a></div>
+<div class="srb3"><a class="a" href="newsmap.html">گوگل مپ با مسیریابی اشتباه ۳ شهروند هندی را به کام مرگ کشاند</a></div>
+<div class="srb3"><a class="a" href="newshamrah.html">چگونه تماس تصویری همراه اول را فعال کنیم؟</a></div>
+<?php
+include("footer.html")
+?>
