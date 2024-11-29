a2fa78296dd28926431aa888f18ab8ac81a4bf68
mojtaba86sh <pcm46home@gmail.com>
2024-11-29 21:18:37 +0400

    Add files via upload

--- magaleh.php
+++ magaleh.php
@@ -2,7 +2,8 @@
 include("header.html")
 ?>
 <p id="td">آموزش های بروز</p>
-<div class="srb3"><a href="magaleh.html">آموزش آپدیت کارت گرافیک AMD</a></div>
+<div class="srb3"><a class="a" href="magalehAMD.html">آموزش آپدیت کارت گرافیک AMD</a></div>
+<div class="srb3"><a class="a" href="magalehNVIDIA.html">آموزش آپدیت کارت گرافیک NVIDIA</a></div>
 <?php
 include("footer.html")
 ?>
